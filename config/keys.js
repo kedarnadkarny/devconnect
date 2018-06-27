e9e6c9fc15776501f27a48eae7011c3181357afe
kedar.nadkarny@gmail.com <kedar.nadkarny@gmail.com>
2018-06-27 09:43:08 -0500

    v1.1

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
-  mongoURI: 'mongodb://kedar:kedardev1@ds163510.mlab.com:63510/devconnection',
-  secretOrKey: 'secret'
-}
-=======
   mongoURI: 'mongoconnection'
 }
->>>>>>> d692807a1c0a94d0f1c1080b0b4bca3520af7ba2
