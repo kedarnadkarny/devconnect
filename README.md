b6330e3008d64f5a8f22458ea5138b515f9e1333
kedar.nadkarny@gmail.com <kedar.nadkarny@gmail.com>
2018-06-30 23:12:39 -0500

    v1.8

---
+++ README.md
@@ -0,0 +1,7 @@
+# Devconnet Backend
+
+This is the backend for the Devconnect social networking site for developers.
+
+The front is built on top of React and Redux which is a separate repository.
+
+Link- [Open Demo](https://devconnect-social.herokuapp.com/register)
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://kedar:kedardev1@ds163510.mlab.com:63510/devconnection',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
